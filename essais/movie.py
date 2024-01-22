6585e60d27a9b00f23d75082abe5d211b014c38e
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-01-22 14:07:26 +0100

    Ajout essais/movie – constructeur

--- essais/movie.py
+++ essais/movie.py
@@ -3,11 +3,20 @@ def durationToString(minutes: int) -> str:
     return f"{minutes // 60}:{minutes % 60}"
 
 
-def ratingToStars(rating: int, max: int):
+def ratingToStars(rating: int, maxi: int):
     res = ''
-    for i in range(max):
+    for i in range(maxi):
         if i < rating:
             res += '\u2605'
         else:
             res += '\u2606'
     return res
+
+
+class Movie:
+    def __init__(self, title: str, duration: int, rating: float = 0):
+        if not (0 <= rating <= 10):
+            raise ValueError("invalid rating for a movie")
+        self._title = title
+        self.duration = duration
+        self.rating = rating
--- essais/test_movie.py
+++ essais/test_movie.py
@@ -1,5 +1,5 @@
 # Fichier créé par Axel COUDROT le 22/01/2024
-from movie import durationToString, ratingToStars
+from movie import durationToString, ratingToStars, Movie
 
 print(durationToString(512))
 print(durationToString(4096))
@@ -9,3 +9,27 @@ print(ratingToStars(0, 8))
 print(ratingToStars(2, 8))
 print(ratingToStars(4, 8))
 print(ratingToStars(8, 8))
+
+m1 = Movie("Les évadés", 510)
+print(vars(m1))
+print(dir(m1))
+
+try:
+    m2: Movie = Movie('Les évadés', 122, 5.5)
+    print('Tout est OK.')
+except:
+    print('Les lignes précédentes n’auraient pas dû lancer d’exception !!')
+try:
+    m2 = Movie('Les évadés', 122, -0.01)
+    print('La ligne précédente aurait dû lancer une exception !!?')
+except ValueError as e:
+    print(f"L’exception \"{e}\" a bien été lancée.")
+except:
+    print("Ce n’est pas la bonne exception qui a été lancée !!?")
+try:
+    m2 = Movie('Les évadés', 122, 10.01)
+    print('La ligne précédente aurait dû lancer une exception !!?')
+except ValueError as e:
+    print(f"L’exception \"{e}\" a bien été lancée.")
+except:
+    print("Ce n’est pas la bonne exception qui a été lancée !!?")
