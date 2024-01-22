ba8e099ff97687d987292d8b5c9139477835e856
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-01-22 13:58:44 +0100

    Ajout de essais/movie – ratingToStars

--- essais/movie.py
+++ essais/movie.py
@@ -1,3 +1,13 @@
 # Fichier créé par Axel COUDROT le 22/01/2024
 def durationToString(minutes: int) -> str:
     return f"{minutes // 60}:{minutes % 60}"
+
+
+def ratingToStars(rating: int, max: int):
+    res = ''
+    for i in range(max):
+        if i < rating:
+            res += '\u2605'
+        else:
+            res += '\u2606'
+    return res
--- essais/test_movie.py
+++ essais/test_movie.py
@@ -1,6 +1,11 @@
 # Fichier créé par Axel COUDROT le 22/01/2024
-from movie import durationToString
+from movie import durationToString, ratingToStars
 
 print(durationToString(512))
 print(durationToString(4096))
 print(durationToString(32768))
+
+print(ratingToStars(0, 8))
+print(ratingToStars(2, 8))
+print(ratingToStars(4, 8))
+print(ratingToStars(8, 8))
