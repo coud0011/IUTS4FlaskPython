1facf461756f561aa732473c4a00cd47c27233b3
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-01-22 14:40:10 +0100

    Ajout de essais/movie – genres

--- essais/movie.py
+++ essais/movie.py
@@ -1,9 +1,12 @@
 # Fichier créé par Axel COUDROT le 22/01/2024
 class Movie:
-    def __init__(self: object, title: str, duration: int = 0, rating: float = 0.0):
+    def __init__(self: object, title: str, duration: int, genres: list, rating: float = 0.0):
+        if not (0 <= rating <= 10):
+            raise ValueError("rating incorrect pour un film")
         self._title = title
         self._duration = duration
-        self.rating = rating
+        self._rating = rating
+        self._genres = genres
 
     # Mise en place de la propriété “title” en lecture seule
     def _getTitle(self) -> str:
@@ -36,6 +39,8 @@ class Movie:
         return self._rating
 
     def _setRating(self, rating: float) -> None:
+        if not (0 <= rating <= 10):
+            raise ValueError("rating incorrect pour un film")
         self._rating = rating
 
     @property
@@ -57,6 +62,19 @@ class Movie:
         """
         self._setRating(r)
 
+    # Mise en place de la propriété “duration” en lecture seule
+    def _getGenres(self) -> list:
+        return self._genres.copy()
+
+    @property
+    def genres(self) -> list:
+        """
+        Retourne la durée du film (exprimée en minutes).
+        Retour :
+        Durée du film
+        """
+        return self._getGenres()
+
     @staticmethod
     def durationToString(minutes: int) -> str:
         return f"{minutes // 60}:{minutes % 60}"
@@ -71,6 +89,17 @@ class Movie:
                 res += '\u2606'
         return res
 
+    def hasGenre(self, genre: str) -> bool:
+        return genre in self.genres
+
     # permet de définir la conversion de l'objet en string
     def __repr__(self) -> str:
-        return f"{self.title} ({Movie.durationToString(self.duration)})\n{Movie.ratingToStars(round(self.rating), 10)}"
+        genres = ''
+        for elt in self._genres:
+            genres += "/" + elt
+        if genres == "":
+            genres = "no genres"
+        else:
+            genres = genres[1:(len(genres) - 1)]
+        return (f"{self.title} ({genres} - {Movie.durationToString(self.duration)})\n"
+                f"{Movie.ratingToStars(round(self.rating), 10)}")
