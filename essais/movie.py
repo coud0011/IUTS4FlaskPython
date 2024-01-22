243d460e6cea5fb4ea837b201a987e60ee3935d7
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-01-22 13:53:25 +0100

    Ajout de essais/movie - durationToString

---
+++ essais/movie.py
@@ -0,0 +1,3 @@
+# Fichier créé par Axel COUDROT le 22/01/2024
+def durationToString(minutes: int) -> str:
+    return f"{minutes // 60}:{minutes % 60}"
---
+++ essais/test_movie.py
@@ -0,0 +1,6 @@
+# Fichier créé par Axel COUDROT le 22/01/2024
+from movie import durationToString
+
+print(durationToString(512))
+print(durationToString(4096))
+print(durationToString(32768))
