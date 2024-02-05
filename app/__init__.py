3d049a77796c539435945e545d615b5d0ff3a972
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-05 16:10:39 +0100

    Ajout de la base de donnée avec sql alchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,18 @@
 # app/__init__.py
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
 from app.config import Config
 
+# Création de l'application
+# __name__ contient le nom de l'application : app
 app = Flask(__name__)
 app.config.from_object(Config)
-
-from app import routes
+# Démarrage du moteur de la base de données
+db = SQLAlchemy(app)
+# Démarrage de l'outil de migration associé à la base de données
+migrate = Migrate(app, db)
+# On importe le fichier contenant
+# la définition des fonctions de vue
+# ainsi que celui des modèles
--- app/config.py
+++ app/config.py
@@ -2,6 +2,11 @@
 # app/config.py
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'un mot de passe à garder secret'
+    # Configuration de la base de données
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
