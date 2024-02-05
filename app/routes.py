2e5e87bfcc30ef6f6078901cdc8c1adb3784f776
coud0011 <axel.coudrot@etudiant.univ-reims.fr>
2024-02-05 13:41:33 +0100

    Ajout des messages postés

--- app/routes.py
+++ app/routes.py
@@ -6,6 +6,17 @@ from app import app
 
 @app.route('/')
 @app.route('/index')
-def index() -> str:
+def index():
     user = {'username': 'Philippe'}
-    return render_template('index.html', title='Page principale', user=user)
+    # return render_template('index.html', title='Page principale', user=user)
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': "Flask, c'est super !"
+        },
+        {
+            'author': {'username': 'Suzan'},
+            'body': "C'est encore mieux que Symfony ! Oups !"
+        }
+    ]
+    return render_template('index.html', title='Accueil', user=user, posts=posts)
